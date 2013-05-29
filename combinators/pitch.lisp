2645142137f194c08b2a12e859ad0473a6cafdc0
Max Rottenkolber <max@mr.gy>
2013-05-29 17:53:39 +0200

    Fixed markup in docstring for PITCH*.

--- combinators/pitch.lisp
+++ combinators/pitch.lisp
@@ -3,11 +3,10 @@
 (in-package :soundlab.combinators)
 
 (defun pitch* (function pitch-function)
-  "Pitch function with PITCH-FUNCTION."
+  "Pitch FUNCTION with PITCH-FUNCTION."
   (lambda (x)
     (funcall function (* x (funcall pitch-function x)))))
 
 (defun pitch (function pitch)
   "Pitch FUNCTION by PITCH."
   (pitch* function (flatline pitch)))
-
