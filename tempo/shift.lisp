c46a02b1cd7bffb8341149605b7cc300edd811b3
Max Rottenkolber <max@mr.gy>
2013-11-01 15:10:54 +0100

    Added docstring to SOUNDLAB.TEMPO:SHIFT.

--- tempo/shift.lisp
+++ tempo/shift.lisp
@@ -3,6 +3,8 @@
 (in-package :soundlab.tempo)
 
 (defun shift (signal offset)
+  "Shift SIGNAL in time by OFFSET. Return amplitude of zero for resulting
+values of time less than zero."
   (lambda (x)
     (let ((xo (- x offset)))
       (if (>= xo 0)
